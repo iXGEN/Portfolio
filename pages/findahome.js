b7af434db8e2a58cb93e2f6efa3aab36404d3f02
iXGEN <106850727+iXGEN@users.noreply.github.com>
2023-05-15 17:24:01 -0300

    desc find a home

--- pages/findahome.js
+++ pages/findahome.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const findahome = () => {
-  return <div>Here are the details of Find A Home Project!</div>;
+  return (
+    <div>
+      <h1>Here are the details of Find A Home Project!</h1>
+      <h2>More details:</h2>
+      <p>Aquí van todas las imágenes del proyecto</p>
+      <p>Descripción detallada</p>
+      <p>descripción de lo que yo hice</p>
+      <p>íconos de las tecnologías</p>
+      <p>Nombres de las tecnologías</p>
+      <p>Sumar todo lo que este en inicio respecto a este proyecto</p>
+    </div>
+  );
 };
 
 export default findahome;
