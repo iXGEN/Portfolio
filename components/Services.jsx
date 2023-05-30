a33b7a4305ab232cab3b9e0c402a3027bb6a1121
iXGEN <106850727+iXGEN@users.noreply.github.com>
2023-05-30 15:29:14 -0300

    upd

--- components/Services.jsx
+++ components/Services.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import Typed from "react-typed";
 
 const Services = () => {
+  const stringsArr = [
+    "As a Full Stack and Mobile Developer, I offer a wide range of services, including:",
+    "Web application development: I can develop web applications using a variety of programming languages, frameworks, and platforms to create highly functional and responsive web applications.",
+    "Mobile application development: I can build mobile applications that are optimized for different platforms like Android and iOS to provide users with a seamless experience.",
+    "API development and integration: I can develop APIs to integrate your software applications with other third-party applications to improve functionality and efficiency.",
+    "Maintenance and support: I can provide ongoing maintenance and support services to ensure that your software applications are running smoothly and efficiently.",
+  ];
+
   return (
     <section id="services" className="pt-20">
       <div>
@@ -10,15 +18,9 @@ const Services = () => {
         </h3>
         <div className="text-md py-2 leading-8 text-gray-800 dark:text-gray-300">
           <Typed
-            strings={[
-              "As a Full Stack and Mobile Developer, I offer a wide range of services, including:",
-              "Web application development: I can develop web applications using a variety of programming languages, frameworks, and platforms to create highly functional and responsive web applications.",
-              "Mobile application development: I can build mobile applications that are optimized for different platforms like Android and iOS to provide users with a seamless experience.",
-              "API development and integration: I can develop APIs to integrate your software applications with other third-party applications to improve functionality and efficiency.",
-              "Maintenance and support: I can provide ongoing maintenance and support services to ensure that your software applications are running smoothly and efficiently.",
-            ]}
-            typeSpeed={10} // Velocidad de escritura en milisegundos
-            backSpeed={10} // Velocidad de borrado en milisegundos
+            strings={stringsArr}
+            typeSpeed={100} // Velocidad de escritura en milisegundos
+            backSpeed={1000} // Velocidad de borrado en milisegundos
             backDelay={1000} // Retraso antes de iniciar el borrado en milisegundos
             loop={false} // No se repetirá la animación
           />
@@ -28,15 +30,22 @@ const Services = () => {
           <Typed
             strings={[
               "I am confident working with a variety of technologies and frameworks such as",
+            ]}
+            typeSpeed={100}
+            backSpeed={10}
+            backDelay={1000}
+            loop={false}
+          />
+          <br></br>
+          <Typed
+            strings={[
               "HTML5, CSS3, Javascript, Wordpress, React, NextJS, React Native, Redux, Tailwind, Bootstrap, NodeJS, ExpressJS, PostgreSQL, MySQL, MongoDB and many more",
             ]}
             typeSpeed={10}
             backSpeed={10}
             backDelay={1000}
-            loop={false} // No se repetirá la animación
+            loop={false}
           />
-
-          <span className="text-teal-500"></span>
         </div>
       </div>
     </section>
